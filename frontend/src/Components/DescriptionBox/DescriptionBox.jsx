8b81177b42e3a3baf6db9d3b3d1b67441a4610ff
nuravision <nurane.ismayilzade19@gmail.com>
2025-01-04 23:22:38 +0400

    some frontend parts completed

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+          <div className="descriptionbox-navigator">
+              <div className="descriptionbox-nav-box">Description</div>
+              <div className="descriptionbox-nav-box fade">Reviews (122) </div>
+          </div>
+          <div className="descriptionbox-description">
+              <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi aut maiores ullam dolore consequuntur laboriosam facere praesentium. Aspernatur perferendis ipsam, cupiditate cumque similique harum inventore assumenda impedit. Ipsam, error delectus?
+              </p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quibusdam, in sunt labore cupiditate id eum illum velit culpa corrupti.</p>
+            
+          </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
